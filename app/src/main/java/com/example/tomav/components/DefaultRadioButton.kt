23f37fe4fcab4164d250e52d1dab4e88c3966e3d
Carlos Melendez <carlosmelendezr@gmail.com>
2023-04-01 19:08:28 -0400

    Cambios 4

---
+++ app/src/main/java/com/example/tomav/components/DefaultRadioButton.kt
@@ -0,0 +1,36 @@
+package com.example.tomav.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun DefaultRadioButton (
+    text:String,
+    selected:Boolean,
+    onSelect:() -> Unit,
+    modifier: Modifier = Modifier
+
+) {
+    Row(
+        modifier=modifier
+    ) {
+        RadioButton(
+            selected = selected,
+            onClick =onSelect,
+        colors = RadioButtonDefaults.colors(
+            selectedColor = MaterialTheme.colors.primary,
+            unselectedColor = MaterialTheme.colors.background
+        ))
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = text,style = MaterialTheme.typography.body1)
+
+    }
+
+}
